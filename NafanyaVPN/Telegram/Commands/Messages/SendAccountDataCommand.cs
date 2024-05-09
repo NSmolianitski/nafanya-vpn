9cb0d38e01faff12a61c84ccb70f5f1cca9a9fc8
Nikita Smolianitski <n.smolianitski@gmail.com>
2024-05-09 11:05:45 +0300

    Add subscription end data to account data command

--- NafanyaVPN/Telegram/Commands/Messages/SendAccountDataCommand.cs
+++ NafanyaVPN/Telegram/Commands/Messages/SendAccountDataCommand.cs
@@ -1,4 +1,5 @@
-﻿using NafanyaVPN.Entities.Telegram.Abstractions;
+﻿using System.Globalization;
+using NafanyaVPN.Entities.Telegram.Abstractions;
 using NafanyaVPN.Entities.Telegram.CommandHandlers.DTOs;
 
 namespace NafanyaVPN.Entities.Telegram.CommandHandlers.Commands.Messages;
@@ -8,7 +9,16 @@ public class SendAccountDataCommand(IReplyService replyService)
 {
     public async Task Execute(MessageDto data)
     {
+        var user = data.User;
+        
+        string subscriptionMessage;
+        if (user.OutlineKey == null || !user.OutlineKey!.Enabled) // TODO: заменить OutlineKey на Subscription
+            subscriptionMessage = "Подписка неактивна";
+        else
+            subscriptionMessage = user.SubscriptionEndDate.ToString(CultureInfo.InvariantCulture);
+        
         await replyService.SendTextWithMainKeyboardAsync(data.Message.Chat.Id, 
-            $"Остаток средств: {data.User.MoneyInRoubles}");
+            $"Остаток средств: {user.MoneyInRoubles}\n" +
+            $"Следующее обновление подписки: {subscriptionMessage}");
     }
 }
