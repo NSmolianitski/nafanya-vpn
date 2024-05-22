28ab30e1cdb8db44a1c6e8cc958d5d1384349dc9
Nikita Smolianitski <n.smolianitski@gmail.com>
2024-05-23 01:57:00 +0300

    Move logs to /app

--- NafanyaVPN/AppBuilderExtensions.cs
+++ NafanyaVPN/AppBuilderExtensions.cs
@@ -50,7 +50,7 @@ public static class AppBuilderExtensions
             .WriteTo.TeleSink(
                 telegramSection[TelegramConstants.LogBotToken], 
                 telegramSection[TelegramConstants.LogBotChatId])
-            .WriteTo.SQLite("logs.db");
+            .WriteTo.SQLite("/app/logs.db");
         
         appBuilder.Logging.AddSerilog(loggerConfiguration.CreateLogger());
     }
