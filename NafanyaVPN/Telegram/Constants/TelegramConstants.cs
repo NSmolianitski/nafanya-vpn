87783a9955d0c2b2ff34e3373d8b6d518459d85b
Nikita Smolianitski <n.smolianitski@gmail.com>
2024-06-02 12:24:34 +0300

    Fix log bot chat id constant

--- NafanyaVPN/Telegram/Constants/TelegramConstants.cs
+++ NafanyaVPN/Telegram/Constants/TelegramConstants.cs
@@ -5,7 +5,7 @@ public static class TelegramConstants
     public const string SettingsSectionName = "Telegram";
     public const string Token = "Token";
     public const string LogBotToken = "LogBotToken";
-    public const string LogBotChatId = "LogsBotChatId";
+    public const string LogBotChatId = "LogBotChatId";
     public const string WebHookUrl = "WebHookUrl";
     public const string WebHookRoute = "WebHookRoute";
     public const string WebHookSecret = "WebHookSecret";
